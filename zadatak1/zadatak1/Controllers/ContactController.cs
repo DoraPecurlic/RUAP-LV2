6ecf4a3aef66f7e288f41ea65421ff147ca95a12
kmatijev <karlo.mat19@gmail.com>
2023-11-06 18:23:50 +0100

    complited Task 3 - Creating the Contact Models and Augment the Contact Controlle

--- zadatak1/zadatak1/Controllers/ContactController.cs
+++ zadatak1/zadatak1/Controllers/ContactController.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using zadatak1.Models;
 
 namespace zadatak1.Controllers
 {
     public class ContactController : Controller
     {
         // GET: Contact
-        public string[] Get()
+        public Contact[] Get()
         {
-            return new string[]
+            return new Contact[]
             {
-                "Hello",
-                "World"
+                new Contact
+                {
+                    Id = 1,
+                    Name = "Glenn Block"
+                },
+                new Contact
+                {
+                    Id = 2,
+                    Name = "Dan Roth"
+                }
+
             };
         }
         public ActionResult Index()
