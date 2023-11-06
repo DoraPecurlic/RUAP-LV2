1b7fa49dc05f19cfadbce61b5a2d915e69a56182
kmatijev <karlo.mat19@gmail.com>
2023-11-06 18:10:07 +0100

    Creating the Contact Manager API Controllers

---
+++ zadatak1/zadatak1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace zadatak1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ zadatak1/zadatak1/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zadatak1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public string[] Get()
+        {
+            return new string[]
+            {
+                "Hello",
+                "World"
+            };
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
